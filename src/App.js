432ff079b7cfeafac95c960b46b76c1cc0ec0fd1
Giovanni Sampaio <giovannibruno1@gmail.com>
2018-02-10 22:02:57 +0000

    Refactor app

--- src/App.js
+++ src/App.js
@@ -5,76 +5,18 @@ import tomato from './tomato.svg';
 class App extends Component {
   constructor() {
     super();
-    this.state = { time: {}, seconds: 25*60, duration: 25, label: "Start" };
+    this.state = { time: {}, seconds: 1500, duration: 25, label: "Start" };
     this.timer = 0;
     this.startTimer = this.startTimer.bind(this);
     this.countDown = this.countDown.bind(this);
     this.resetTimer = this.resetTimer.bind(this);
   }
 
-  // take seconds as input and formats the data
-  secondsToTime(secs){
-
-    // copy to a helpers file
-    function pad(val) {
-      var valString = val + "";
-      
-      if (valString.length < 2) {
-        return "0" + valString;
-      } else {
-        return valString;
-      }
-    };
-
-    let hours = pad(Math.floor(secs / (60 * 60)));
-
-    let divisor_for_minutes = secs % (60 * 60);
-    let minutes = pad(Math.floor(divisor_for_minutes / 60));
-
-    let divisor_for_seconds = divisor_for_minutes % 60;
-    let seconds = pad(Math.ceil(divisor_for_seconds));
-
-    let obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
-    };
-    return obj;
-  }
-
-  minutesToTime(min) {
-    // copy to a helpers file
-    function pad(val) {
-      var valString = val + "";
-      
-      if (valString.length < 2) {
-        return "0" + valString;
-      } else {
-        return valString;
-      }
-    };
-    let secs = min * 60
-    let hours = pad(Math.floor(secs / (60 * 60)));
-
-    let divisor_for_minutes = secs % (60 * 60);
-    let minutes = pad(Math.floor(divisor_for_minutes / 60));
-
-    let divisor_for_seconds = divisor_for_minutes % 60;
-    let seconds = pad(Math.ceil(divisor_for_seconds));
-
-    let obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
-    };
-    return obj;
-  }
-
   // is invoked immediately after a component is mounted
   // it takes the 'seconds' in state, converts it and pass it to 'time' also in state 
   componentDidMount() {
     // save the current time in a var
-    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    let timeLeftVar = secondsToTime(this.state.seconds);
     // set the time in state
     this.setState({ time: timeLeftVar });
   }
@@ -98,10 +40,10 @@ class App extends Component {
     clearInterval(this.timer)
 
     let duration = this.state.duration
-    let timeLeftVar = this.minutesToTime(duration);
+    let timeLeft = minutesToTime(duration);
     let seconds = this.state.duration * 60
     // reset the state
-    this.setState({seconds: seconds, time: timeLeftVar, label: "Start"})
+    this.setState({seconds: seconds, time: timeLeft, label: "Start"})
     this.timer = 0
   }
 
@@ -112,12 +54,11 @@ class App extends Component {
 
   countDown() {
 
-    // Remove one second 
     let seconds = this.state.seconds - 1;
     // set state so a re-render happens, which triggers componentDidMount()
     this.setState({
       // update time and seconds with 'seconds -1'
-      time: this.secondsToTime(seconds),
+      time: secondsToTime(seconds),
       seconds: seconds,
     });
     
@@ -136,10 +77,9 @@ class App extends Component {
         <button className="waves-effect waves-teal btn-flat" onClick={this.startTimer}>{this.state.label}</button>
         <button className="waves-effect waves-teal btn-flat" onClick={this.resetTimer}>Reset</button>
         <form>
-          <label>
-            Pomodoro duration:
-            <input type="number" min="1" className="duration" value={this.state.duration} onChange={this.changeDuration} />
-          </label>
+            <label> Pomodoro Duration
+                <input id="duration" type="number" min="1" value={this.state.duration} onChange={this.changeDuration} />
+            </label>
           {/*<input type="submit" value="Change" />/*/}
         </form>
       </div>
@@ -148,3 +88,61 @@ class App extends Component {
 }
 
 export default App;
+
+//HELPERS
+// copy to a helpers file
+function pad(val) {
+  var valString = val + "";
+    
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+      return valString;
+  }
+};
+
+  // take seconds as input and formats the data
+function  secondsToTime(secs) {
+  let hours = pad(Math.floor(secs / (60 * 60)));
+
+  let divisor_for_minutes = secs % (60 * 60);
+  let minutes = pad(Math.floor(divisor_for_minutes / 60));
+
+  let divisor_for_seconds = divisor_for_minutes % 60;
+  let seconds = pad(Math.ceil(divisor_for_seconds));
+
+  let obj = {
+    "h": hours,
+    "m": minutes,
+    "s": seconds
+  };
+  return obj;
+}
+
+function minutesToTime(min) {
+  // copy to a helpers file
+  function pad(val) {
+    var valString = val + "";
+      
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  };
+  let secs = min * 60;
+  let hours = pad(Math.floor(secs / (60 * 60)));
+
+  let divisor_for_minutes = secs % (60 * 60);
+  let minutes = pad(Math.floor(divisor_for_minutes / 60));
+
+  let divisor_for_seconds = divisor_for_minutes % 60;
+  let seconds = pad(Math.ceil(divisor_for_seconds));
+
+  let obj = {
+    "h": hours,
+    "m": minutes,
+    "s": seconds
+  };
+  return obj;
+}
